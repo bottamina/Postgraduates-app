ae5751b652a7936293cd879b92bcf812a2f1431a
kasklezneva <91009384+kasklezneva@users.noreply.github.com>
2022-12-01 21:10:19 +0300

    Update app.R

--- app.R
+++ app.R
@@ -153,27 +153,3 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui = ui, server = server)
-
-# library(shiny)
-# 
-# ui <- navbarPage(
-#   "Page title",
-#   tabPanel("panel 1", "one"),
-#   tabPanel("panel 2", "two"),
-#   tabPanel("panel 3", "three"),
-#   navbarMenu("subpanels",
-#              tabPanel("panel 4a", "four-a"),
-#              tabPanel("panel 4b", "four-b"),
-#              tabPanel("panel 4c", "four-c")
-#   )
-# )
-# 
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {
-# 
-#     
-# }
-# 
-# # Run the application 
-# shinyApp(ui = ui, server = server)
-#fhfhyfhufhuf
